eec5540321c822b7f0c562ef057f5f1282822b67
Martin Fieber <info@martin-fieber.de>
2017-12-26 17:44:49 +0100

    refactor(acai): Show results for all files
    The method onlyUseTopResults() is now deprecated and will not be used anymore to reduce the results.

--- packages/acai/src/scanner.js
+++ packages/acai/src/scanner.js
@@ -14,7 +14,6 @@ const spots = require('./spots');
  * @property {number} [depth=Infinity] Scan depth or "how many commits in the past"
  * @property {RegExp} [pattern=/^(?:(?!branch.+into\s'master').)*\bfix(?:ed|es)?|close(?:s|d)?\b/i] An expression to match commit messages and excludes master merges
  * @property {string} [fileGlob=*] A glob pattern for which files should be considered
- * @property {number} [usage=80] Number in percent of top hot-spots that should be considered. The top 80% strips out the lower noise.
  * @property {string} [branchName=master] The branch name to scan
  */
 
@@ -38,8 +37,7 @@ module.exports = async function scanner(
         branchName = 'master',
         depth,
         fileGlob = '*',
-        pattern = /^(?:(?!branch.+into 'master').)*\bfix(?:ed|es)?|close(?:s|d)?\b/i,
-        usage = 80
+        pattern = /^(?:(?!branch.+into 'master').)*\bfix(?:ed|es)?|close(?:s|d)?\b/i
     } = {}
 ) {
     const commits = await Promise.all(
@@ -51,11 +49,8 @@ module.exports = async function scanner(
             .map(async commit => await git.getDescriptor(commit))
     );
 
-    const usageAsNormalizedValue = usage / 100;
     const fixes = commit.filterByFileGlob(commits, fileGlob);
-    const hotspots = spots.normalize(
-        spots.onlyUseTopResults(spots.calculate(fixes), usageAsNormalizedValue)
-    );
+    const hotspots = spots.normalize(spots.calculate(fixes));
 
     return { fixes, hotspots };
 };
