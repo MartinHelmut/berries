4980579b83713a03cd08cbdee5c32af619e86704
Martin Fieber <info@martin-fieber.de>
2018-01-30 15:28:38 +0100

    fix: fix wrong branch resolution

--- packages/acai/src/adapter/git.js
+++ packages/acai/src/adapter/git.js
@@ -35,6 +35,8 @@ module.exports = {
     async getCommits({ path, branch, depth }) {
         const repo = await nodegit.Repository.open(path);
 
+        console.log('branch:', branch);
+
         if (typeof branch === 'string') {
             await repo.checkoutBranch(branch);
         }
