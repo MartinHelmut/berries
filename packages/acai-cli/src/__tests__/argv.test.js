28b617f2a95d3113c5702f44ad26ee622d06980e
Martin Fieber <info@martin-fieber.de>
2018-02-08 20:39:39 +0100

    test(acai-cli): add argv tests for parameter

--- packages/acai-cli/src/__tests__/argv.test.js
+++ packages/acai-cli/src/__tests__/argv.test.js
@@ -14,12 +14,40 @@ describe('argv', () => {
             );
         });
 
-        test('"cwd" option is defined', () => {
+        test('"cwd" option can be defined', () => {
             expect(argv('--cwd="foo/path"').cwd).toBe('foo/path');
+        });
+
+        test('"cwd" option can be defined by shortcut "d"', () => {
+            expect(argv('-d="foo/path"').cwd).toBe('foo/path');
         });
 
         test('"format" option defines a default', () => {
             expect(argv().format).toBe('human');
+        });
+
+        test('"format" option can be defines by shortcut "F"', () => {
+            expect(argv('-F human').format).toBe('human');
+        });
+
+        test('"branch" option can be defined', () => {
+            expect(argv('--branch master').branch).toBe('master');
+        });
+
+        test('"branch" option can be defined by shortcut "b"', () => {
+            expect(argv('-b master').branch).toBe('master');
+        });
+
+        test('"depth" option can be defined as a number', () => {
+            expect(argv('--depth 10').depth).toBe(10);
+        });
+
+        test('"depth" option will be converted to a number', () => {
+            expect(argv('--depth "10"').depth).toBe(10);
+        });
+
+        test('"depth" option can be defined by shortcut "D"', () => {
+            expect(argv('-D 42').depth).toBe(42);
         });
     });
 });
