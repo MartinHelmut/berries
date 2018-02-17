74ea28a8c3bed2d5fa45d011664531d20f253e1e
Martin Fieber <info@martin-fieber.de>
2018-02-17 17:27:59 +0100

    fix(acai): add correct TypeScript typings

--- packages/acai/index.d.ts
+++ packages/acai/index.d.ts
@@ -8,14 +8,29 @@ declare function scanner(repoPath: string, options?: scanner.ScannerOptions): sc
 
 declare namespace scanner {
 	export interface ScannerOptions {
-		branchName?: string;
+		branch?: string;
 		depth?: number;
-		fileGlob?: string;
+		files?: string[];
 		dispatch?: dispatchCallback;
 		pattern?: RegExp;
 	}
 
-	export interface ScannerResult {}
+	export interface Commit {
+		message: string;
+		time: number;
+		files: string[];
+	}
+
+	export interface HotSpot {
+		file: string;
+		score: number;
+	}
+
+	export interface ScannerResult {
+		fixes: Commit[];
+		hotSpots: HotSpot[];
+		time: number;
+	}
 
 	export type dispatchCallback = <P, M>(action: Action<P, M>) => void;
 
