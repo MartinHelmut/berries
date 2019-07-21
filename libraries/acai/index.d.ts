9ed0c0d8684d543e8c9995d1b71f37e42f274bf8
Martin Helmut Fieber <info@martin-fieber.de>
2019-07-21 19:28:46 +0200

    style(libraries/acai): format files

--- libraries/acai/index.d.ts
+++ libraries/acai/index.d.ts
@@ -4,49 +4,52 @@
 
 export = scanner;
 
-declare function scanner(repoPath: string, options?: scanner.ScannerOptions): scanner.ScannerResult;
+declare function scanner(
+  repoPath: string,
+  options?: scanner.ScannerOptions
+): scanner.ScannerResult;
 
 declare namespace scanner {
-	export interface ScannerOptions {
-		branch?: string;
-		depth?: number;
-		files?: string[];
-		dispatch?: dispatchCallback;
-		pattern?: RegExp;
-	}
+  export interface ScannerOptions {
+    branch?: string;
+    depth?: number;
+    files?: string[];
+    dispatch?: dispatchCallback;
+    pattern?: RegExp;
+  }
 
-	export interface Commit {
-		message: string;
-		time: number;
-		files: string[];
-	}
+  export interface Commit {
+    message: string;
+    time: number;
+    files: string[];
+  }
 
-	export interface HotSpot {
-		file: string;
-		score: number;
-	}
+  export interface HotSpot {
+    file: string;
+    score: number;
+  }
 
-	export interface ScannerResult {
-		fixes: Commit[];
-		hotSpots: HotSpot[];
-		time: number;
-	}
+  export interface ScannerResult {
+    fixes: Commit[];
+    hotSpots: HotSpot[];
+    time: number;
+  }
 
-	export type dispatchCallback = <P, M>(action: Action<P, M>) => void;
+  export type dispatchCallback = <P, M>(action: Action<P, M>) => void;
 
-	export interface Action<P = {}, M = {}> {
-		type: ActionType;
-		payload: P;
-		meta?: M;
-	}
+  export interface Action<P = {}, M = {}> {
+    type: ActionType;
+    payload: P;
+    meta?: M;
+  }
 
-	export enum ActionType {
-		LOG = 'LOG',
-		START = 'START',
-		COMMIT = 'COMMIT',
-		COMMITS = 'COMMITS',
-		FIXES = 'FIXES',
-		HOTSPOTS = 'HOTSPOTS',
-		END = 'END'
-	}
+  export enum ActionType {
+    LOG = "LOG",
+    START = "START",
+    COMMIT = "COMMIT",
+    COMMITS = "COMMITS",
+    FIXES = "FIXES",
+    HOTSPOTS = "HOTSPOTS",
+    END = "END"
+  }
 }
